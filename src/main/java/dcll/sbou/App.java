6c3a67d91f1e80dfdf275fb01e80bb5ae8158eb2
sebastien-bouyt <sebastienbouyt@hotmail.fr>
2016-04-14 21:45:18 +0200

    test

---
+++ src/main/java/dcll/sbou/App.java
@@ -0,0 +1,13 @@
+package dcll.sbou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
